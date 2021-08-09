16aa2f056c7ac52e0dca48c52650c5bc6bc1676e
Alessio Giacomel <alessio.giacomel@kcl.ac.uk>
2021-08-09 16:01:41 +0200

    update init

--- imaging_transcriptomics/__init__.py
+++ imaging_transcriptomics/__init__.py
@@ -6,6 +6,7 @@ from . import bootstrap
 from . import inputs
 from . import permutations
 from . import transcriptomics
+from . import genes
 
 from .transcriptomics import ImagingTranscriptomics
 from .bootstrap import (bootstrap_pls,
@@ -15,3 +16,4 @@ from .inputs import (load_gene_expression,
                      get_components,
                      read_scan,
                      extract_average)
+from .genes import GeneResults
