92a5773725f4c7bb8a6f53d489cc85e0fd21ecf7
Alessio Giacomel <alessio.giacomel@kcl.ac.uk>
2021-08-13 17:51:43 +0200

    changes to setup

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+from glob import glob
 
 
 def read_long_description():
@@ -6,23 +7,37 @@ def read_long_description():
 
     :return: contents of the readme file.
     """
-    with open("README.rst", "r") as f:
+    with open("README.md", "r") as f:
         return f.read()
 
 
 def get_version_number():
+    """Get the version of the package from the __init__ file.
+
+    :return version: version of the package.
+    """
     with open("imaging_transcriptomics/__init__.py", "r") as f:
         line = f.readline()
         version = line.split(" = ")[1]
         return version
 
 
+def get_requirements():
+    """Get the requirements for the installation."""
+    required = []
+    with open("requirements.txt", "r") as f:
+        for line in f:
+            required.append(line.strip("\n"))
+    return required
+
+
 setup(name="imaging-transcriptomics",
       author="Daniel Martins, MD, PhD; Alessio Giacomel",
       author_email=["daniel.martins@kcl.ac.uk", "alessio.giacomel@kcl.ac.uk"],
       version=get_version_number(),
-      description="Analyse an imaging scan with imaging transcriptomics",
+      description="A package to perform imaging transcriptomics on a neuroimaging brain scan.",
       long_description=read_long_description(),
+      long_description_content_type="text/markdown",
       classifiers=["Intended Audience :: Healthcare Industry",
                    "Intended Audience :: Science/Research",
                    "Topic :: Scientific/Engineering :: Image Processing",
@@ -32,6 +47,9 @@ setup(name="imaging-transcriptomics",
                    "Programming Language :: Python :: 3.6",
                    ],
       keywords="Image analysis, Neuroimaging, Imaging Transcriptomics, Medical Imaging, Research, Multimodal Imaging",
+      install_requires=get_requirements(),
       packages=find_packages(),
+      include_package_data=True,
+      scripts=glob("scripts/*"),
       entry_points={"console_scripts": ["imaging-transcriptomics = scripts.imagingtranscriptomics:main"]}
       )
