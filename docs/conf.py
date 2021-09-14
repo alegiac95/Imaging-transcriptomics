bf46869db6c2fca606a5df1c8a9fdce3c8a11b0b
Alessio Giacomel <alessio.giacomel@kcl.ac.uk>
2021-09-14 13:26:17 +0200

    changes and added faq

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../imaging_transcriptomics/'))
+sys.path.insert(0, os.path.abspath('../../imaging_transcriptomics/'))
 
 
 # -- Project information -----------------------------------------------------
