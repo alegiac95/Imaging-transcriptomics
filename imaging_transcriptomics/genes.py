analysis works, need fix on genes value computation

imaging_transcriptomics/genes.py
@@ -66,8 +66,8 @@ class BootResults(dict):
         """Compute the values of the bootstrap for each of the components.
 
         :param n_comp: number of PLS components.
-        :param original_weights: weights obtained from the original analysis (non bootstrapped).
-        :param original_ids: original ids (labels) from the original analysis (non bootstrapped).
+        :param original_weights: weights obtained from the original analysis (not bootstrapped)
+        :param original_ids: original ids (labels) from the original analysis (not bootstrapped)
         :return:
         """
         logger.info("Computing bootstrap gene results.")
@@ -86,6 +86,30 @@ class BootResults(dict):
                 __p[::-1].reshape(1, 15633), method="fdr_bh", is_sorted=True
             )
             self.pval_corrected[component - 1] = __p_corr
+
+    def compute_corr(self, original_corr, originals_ids, original_index):
+        """Compute bootstrapping values for correlation data.
+        :param
+        """
+        logger.info("Computing bootstrap correlation results")
+        n_iter = 1000
+        for i in range(n_iter):
+            self.pls_weights_boot[:, i] = self.pls_weights_boot[:, i][
+                original_index
+            ]
+        p = np.zeros(15633)
+        for i in range(15633):
+            p[i] = (
+                float(
+                    len(np.nonzero(self.pls_weights_boot[i, :] >= original_corr[i]))
+                )
+                / n_iter
+            )
+        self.pval = p
+        _, p_corrected, _ ,_= multipletests(p[::-1], method="fdr_bh",
+                                           is_sorted=True)
+        self.pval_corrected = p_corrected
+        self.pls_genes = originals_ids[original_index]
 
 
 class GeneResults(dict):

imaging_transcriptomics/transcriptomics.py
@@ -7,7 +7,7 @@ import numpy as np
 import pandas as pd
 import yaml
 from pyls import pls_regression
-from scipy.stats import zscore, pearsonr
+from scipy.stats import zscore, spearmanr
 
 with warnings.catch_warnings():
     warnings.filterwarnings("ignore")
@@ -175,7 +175,7 @@ class ImagingTranscriptomics:
         pd.DataFrame(self.permuted).to_csv(Path(path), header=None, index=False)
 
     def correlation(self):
-        """ Calculate the correlation between the imaging and genetic data.
+        """Calculate the correlation between the imaging and genetic data.
 
         :return corr_genes: pearson correlation coefficient ordered in
         descending order.
@@ -184,8 +184,9 @@ class ImagingTranscriptomics:
         corr_ = np.zeros(self._gene_expression.shape[1])
         p_val = np.zeros(self._gene_expression.shape[1])
         for gene in range(15633):
-            corr_[gene], p_val[gene] = pearsonr(self.zscore_data,
-                                                    self._gene_expression[:,gene])
+            corr_[gene], p_val[gene] = spearmanr(
+                self.zscore_data, self._gene_expression[:, gene]
+            )
         corr_genes = np.sort(corr_)
         corr_gene_labels = self._gene_labels[np.argsort(corr_)]
         return corr_genes, corr_gene_labels
@@ -252,12 +253,21 @@ class ImagingTranscriptomics:
             # run first analysis
             self.permute_data(iterations=n_iter)
             # bootstrap analysis
-            self.gene_results = bootstrap_correlation(self.zscore_data,
-                                                      self._gene_results,
-                                                      self._gene_labels)
-            pass
+            self.gene_results = bootstrap_correlation(
+                self.zscore_data,
+                self._gene_expression,
+                self.permuted,
+                self._gene_labels,
+            )
+            self.gene_results.boot_results.compute_corr(
+                self.gene_results.original_results.pls_weights,
+                self.gene_results.original_results.pls_gene,
+                self.gene_results.original_results.gene_id,
+            )
         else:
-            raise NotImplementedError(f"The method {method} does not exist. "
-                                      f"Please choose either pls or corr as "
-                                      f"method to run the analysis.")
+            raise NotImplementedError(
+                f"The method {method} does not exist. "
+                f"Please choose either pls or corr as "
+                f"method to run the analysis."
+            )
         logger.info("Imaging transcriptomics analysis completed.")