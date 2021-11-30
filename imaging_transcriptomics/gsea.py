dcdb6e3452435e1475806021e56557138e1ad2ce
Alessio Giacomel <alessio.giacomel@kcl.ac.uk>
2021-11-30 17:08:40 +0000

    added file with refactored class - start

--- imaging_transcriptomics/gsea.py
+++ imaging_transcriptomics/gsea.py
@@ -1,14 +1,21 @@
 import gseapy
 import pandas as pd
+from pathlib import Path
 
 
-def gsea(gene_names, gene_scores, gene_set=None, out_dir=None, fmt="png",
+def gsea(gene_names, gene_scores, gene_set: str = None, out_dir=None,
+         fmt="png",
          no_plot=False):
     """Perform pre-rank GSEA on a set of gene labels and scores."""
     # Create a dataframe with the gene names and scores
 
     rnk = pd.DataFrame(list(zip(gene_names, gene_scores)))
-    gene_set = gene_set
+    if gene_set.lower() == "lake":
+        gene_set = Path(__file__).parent.parent / "data" / "geneset_LAKE.gmt"
+    elif gene_set.lower() == "pooled":
+        gene_set = Path(__file__).parent.parent / "data" / "geneset_Pooled.gmt"
+    else:
+        gene_set = gene_set
     # Perform GSEA
     gsea_results = gseapy.prerank(rnk=rnk,
                                   gene_set=gene_set,
@@ -17,3 +24,8 @@ def gsea(gene_names, gene_scores, gene_set=None, out_dir=None, fmt="png",
                                   format=fmt,
                                   no_plot=no_plot)
     return gsea_results
+
+
+def ensemble_gsea(gene_names, gene_scores, permuted_gene_names,
+                  permuted_gene_labels):
+    pass
