5eb3ea9da9b487902be511c59cd06488e9124d81
alegiac95 <alessiogiacomel@outlook.com>
2026-03-30 17:18:49 +0200

    docs: switch site theme to furo

--- docs/conf.py
+++ docs/conf.py
@@ -52,9 +52,15 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'furo'
+html_theme_options = {
+    "light_logo": "imaging_transcriptomics_toolbox_logo.svg",
+    "dark_logo": "imaging_transcriptomics_toolbox_logo.svg",
+    "sidebar_hide_name": True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = ['custom.css']
